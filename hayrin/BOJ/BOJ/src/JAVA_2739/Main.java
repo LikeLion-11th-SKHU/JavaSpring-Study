bb17e7fd68af159d0c309ee8fbd2238b1171ac37
hayrin0603 <hinoerak9@gmail.com>
2023-09-19 03:12:34 +0900

    2주차 과제 제출

---
+++ hayrin/BOJ/BOJ/src/JAVA_2739/Main.java
@@ -0,0 +1,16 @@
+package JAVA_2739;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        in.close();
+
+        for(int i=1; i<10; i++){
+            System.out.println(N+" * "+i+" = "+(N*i));
+        }
+    }
+}
