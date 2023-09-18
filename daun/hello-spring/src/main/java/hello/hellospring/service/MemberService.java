eb7daa46698764366d4b026fb8c023a716bd89fe
wjdekdns1028 <wjdekdns0218@naver.com>
2023-09-18 19:19:22 +0900

    week 2 assignment

--- daun/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ daun/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,9 +5,11 @@ import hello.hellospring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -19,10 +21,19 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member){
+
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
         //같은 이름이 있는 중복 회원X
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -36,9 +47,9 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+            return memberRepository.findAll();
     }
-    public Optional<Member> findOne(Long memberId){
-        return memberRepository.findById(memberId);
+    public Optional<Member> findOne(Long memberId)
+        {return memberRepository.findById(memberId);
     }
 }
