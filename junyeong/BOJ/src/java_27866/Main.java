db6e0ceae1b9149866dc21758e7748d542884c6c
treaty321 <2117ab@gmail.com>
2023-09-26 03:55:30 +0900

    3ã3week Assignment

---
+++ junyeong/BOJ/src/java_27866/Main.java
@@ -0,0 +1,16 @@
+package java_27866;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String st = sc.next();
+        int n = sc.nextInt()-1;
+
+        System.out.println(st.charAt(n));
+
+        sc.close();
+    }
+}
